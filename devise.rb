d6ec8c17f8c6da90d04bf378a1c048b78bc079f1
dogaruemiliano <dogaru.emiliano@gmail.com>
2021-02-21 16:18:22 +0200

    Added zip and unziped master stylesheet

--- devise.rb
+++ devise.rb
@@ -83,7 +83,7 @@ end
 # README
 ########################################
 markdown_file_content = <<-MARKDOWN
-Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+Rails app generated with [dogaruemiliano/rails-templates](https://github.com/dogaruemiliano/rails-TEMPLATES).
 MARKDOWN
 file 'README.md', markdown_file_content, force: true
 
@@ -209,7 +209,7 @@ after_bundle do
   # Git
   ########################################
   git add: '.'
-  git commit: "-m 'Initial commit with devise template from https://github.com/lewagon/rails-templates'"
+  git commit: "-m 'Initial commit with devise template from https://github.com/dogaruemiliano/rails-TEMPLATES'"
 
   # Fix puma config
   gsub_file('config/puma.rb', 'pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }', '# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }')
