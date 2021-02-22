d08868d5ebbf444123f0b6cbeac896a3a1c0ba49
dogaruemiliano <dogaru.emiliano@gmail.com>
2021-02-22 18:50:35 +0200

    Added webpacker:react installation

--- react_redux.rb
+++ react_redux.rb
@@ -199,9 +199,11 @@ after_bundle do
   environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
   environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
 
-  # React & Redux
+  # React & Redux + dependencies
   ########################################
-
+  rails_command 'webpacker:install'
+  rails_command 'webpacker:install:react'
+  generate('react:install')
 
   # Webpacker / Yarn
   ########################################
